dcfbd2b0fed8e887e872050f0c163834fc4a22a5
alex85vol <alex5vol@gmail.com>
2017-01-04 18:28:15 +0200

    new files

---
+++ 4 Triangle/FirstConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.SmallBasic.Library;
+
+namespace FirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GraphicsWindow.Show();
+            int x = 100, y = 100;
+            var Rnd = new Random();
+            for (int i = 0; i < 100000; i++)
+            {
+                GraphicsWindow.SetPixel(x, y, "Black");
+                var r = Rnd.Next(1, 30);
+                if (r < 10) Move(ref x, ref y, 100, 500);
+                else if (r < 20) Move(ref x, ref y, 600, 500);
+                else Move(ref x, ref y, 350, 100);
+            }
+        }
+
+        static void Move(ref int x, ref int y, int nx, int ny)
+        {
+            x = (x + nx) / 2;
+            y = (y + ny) / 2;
+        }
+
+    }
+}

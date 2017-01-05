572a03cf48615dea6db9e705ca270d878566ca83
alex85vol <alex85vol@gmail.com>
2017-01-05 19:07:17 +0200

    new

---
+++ KohSnow/KohSnow/BasicDraw.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
+
+namespace KohSnow
+{
+    class BasicDraw
+    {
+        public static void basicDraw(int len, int angle)
+        {
+            Turtle.Turn(2 * angle);
+            Turtle.Move(len);
+            Turtle.Turn(-angle);
+            Turtle.Move(len);
+            Turtle.Turn(2 * angle);
+            Turtle.Move(len);
+            Turtle.Turn(-angle);
+            Turtle.Move(len);
+        }
+    }
+}
---
+++ KohSnow/KohSnow/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
+
+namespace KohSnow
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            int len = 100;
+            int angle = 45;
+            BasicDraw.basicDraw(len, angle);
+
+        }
+    }
+}

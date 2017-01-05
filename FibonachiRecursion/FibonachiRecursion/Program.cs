ef642ea5287add3c3d579f14b58bae0f2bb8a029
alex85vol <alex85vol@gmail.com>
2017-01-05 15:21:13 +0200

    first redursion

---
+++ FibonachiRecursion/FibonachiRecursion/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonachiRecursion
+{
+    class Program
+    {
+       /* static int fibonachiNumbers(int n)
+        {
+            if ( n < 2 ) return 1;
+            else return fibonachiNumbers(n - 1) + fibonachiNumbers(n - 2);
+        }*/
+        static void Main(string [] args)
+        {
+            /*for ( int i = 1; i < 50; i++ )
+            {
+                Console.WriteLine("{0}", fibonachiNumbers(i));
+            }*/
+
+            int u = 1, v = 1;
+            for ( int i = 0; i < 40; i++ )
+            {
+                Console.WriteLine("{0}", u);
+                int w = u + v;
+                u = v;
+                v = w;
+            }
+
+            Console.ReadKey();
+            
+        }
+    }
+}
--- FibonachiRecursion/FibonachiRecursion/Properties/AssemblyInfo.cs
+++ FibonachiRecursion/FibonachiRecursion/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HouseDraw")]
+[assembly: AssemblyTitle("FibonachiRecursion")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HouseDraw")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("FibonachiRecursion")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a7bc4fcf-dd86-47c7-9063-13eb26449aea")]
+[assembly: Guid("acbe658f-ab27-41f6-8612-4d244ca7c820")]
 
 // Version information for an assembly consists of the following four values:
 //
